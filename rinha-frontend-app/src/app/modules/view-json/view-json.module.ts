2d6352eb7a20463305c5f94e1dc764f752721bb9
Fabiana Tavares <fabi.tavares1@gmail.com>
2023-10-14 17:09:01 -0300

    project angular

---
+++ rinha-frontend-app/src/app/modules/view-json/view-json.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ViewJsonRoutingModule } from './view-json-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ViewJsonRoutingModule
+  ]
+})
+export class ViewJsonModule { }
